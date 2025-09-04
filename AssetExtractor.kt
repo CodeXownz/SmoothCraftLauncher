c0ae7c97dc6d74cd2fb93426ec961c0fc56baee1
CodeXownz <begamingcodex@gmail.com>
2025-09-04 09:19:46 +0545

    Create AssetExtractor.kt

---
+++ AssetExtractor.kt
@@ -0,0 +1,10 @@
+package com.smoothcraft.launcher
+
+import android.content.Context
+import java.io.File
+
+object AssetExtractor {
+    fun ensureExtracted(ctx: Context) {
+        // TODO: Extract OpenJDK and LWJGL files from assets
+    }
+}
